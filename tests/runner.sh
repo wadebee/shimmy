c0d1472b1e7b99dd381d98dbf07db86ab12c5203
beewa <wade.bee@ngc.com>
2026-09-17 09:22:12 -0400

    test(keepassxc): register keepassxc tool tests in runner and test suite

--- tests/runner.sh
+++ tests/runner.sh
@@ -41,6 +41,7 @@ tools-gdrive|test_tools_gdrive_run
 tools-gh|test_tools_gh_run
 tools-go|test_tools_go_run
 tools-jq|test_tools_jq_run
+tools-keepassxc|test_tools_keepassxc_run
 tools-jv|test_tools_jv_run
 tools-netcat|test_tools_netcat_run
 tools-nmap|test_tools_nmap_run
@@ -99,6 +100,7 @@ tools-gdrive|two-a|three-c
 tools-gh|two-b|three-c
 tools-go|two-b|three-c
 tools-jq|two-a|three-c
+tools-keepassxc|two-a|three-c
 tools-jv|two-b|three-c
 tools-netcat|two-a|three-c
 tools-nmap|two-a|three-c
--- tests/test.sh
+++ tests/test.sh
@@ -139,6 +139,8 @@ TEST_COUNT=0
 . "$ROOT_DIR/tools/go/tests/go.sh"
 # shellcheck source=tools/jq/tests/jq.sh
 . "$ROOT_DIR/tools/jq/tests/jq.sh"
+# shellcheck source=tools/keepassxc/tests/keepassxc.sh
+. "$ROOT_DIR/tools/keepassxc/tests/keepassxc.sh"
 # shellcheck source=tools/jv/tests/jv.sh
 . "$ROOT_DIR/tools/jv/tests/jv.sh"
 # shellcheck source=tools/netcat/tests/netcat.sh
